06e4538ccda0553ead940f75a89879c1f6ce529d
angel794 <angelhdzsv@gmail.com>
2026-08-14 15:44:28 -0600

    removed node_modules from javascript action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,9 +1,14 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const exec = require('@actions/exec');
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
-function run() {
-    core.notice('Hello from my custom JavaScript Action!');
+// function run() {
+//     core.notice('Hello from my custom JavaScript Action!');
+// }
+
+// run();
+
+try {
+  core.notice('Hello from my custom JavaScript Action!');
+} catch (error) {
+  core.setFailed(error.message);
 }
-
-run();
